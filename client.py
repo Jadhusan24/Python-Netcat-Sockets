77cc7c3110ba273730a8f19e81d169c5b93a1679
Jadhusan-Sadhik <54207086+Jadhusan-S@users.noreply.github.com>
2021-05-07 12:10:26 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,32 +1,57 @@
-#!/usr/bin/python
-#COHNDNE201f-021
-
+#!/usr/bin/python3
 import socket
 import threading
-from module import handle_conn, send_message, ADDR
-#sockets - https://docs.python.org/3/library/socket.html
-#threading - https://realpython.com/intro-to-python-threading/
+import sys, time
+from module import handle_conn, send_message
 
-def main():
+
+def main(port: int, server: str):
     try:
-        # create socket
         client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client.connect(ADDR)  # connect to an address
-
+        client.connect((server, port))
+        print("Connection establised to host. ")
+        get_msg = threading.Thread(target=handle_conn, args=(
+            client, (server, port)), daemon=True)
+        send_msg = threading.Thread(
+            target=send_message, args=(client,))
+        
+        get_msg.setDaemon(True)
+        send_msg.setDaemon(True)
         # Here, I used threading to asynchronously receive and send data between the client and server.
         # Threading simply creates a separate set of threads in memory. This helps the script to run without
         # having to wait for loops to end
-        get_msg = threading.Thread(target=handle_conn,args=(client, ADDR))  # this thread is used for receiving messages/data
-        send_msg = threading.Thread(target=send_message,args=(client,))  # this thread is used for sending messages/data
+        get_msg.start() # this thread is used for receiving messages/data
+        send_msg.start()  # this thread is used for sending messages/data
 
-        # start both threads
-        get_msg.start()
-        send_msg.start()
+        while True:
+            for _ in range(10):
+                time.sleep(.2)
+    except KeyboardInterrupt:
+        print("\nDisconneccting from server")
+        exit(1)
     except ConnectionRefusedError:
         print("[CONN ERR] Server refused connection!")
+        exit()
     except Exception as e:
         print(f"[ERR] {e}")
+        client.close()
+        exit()
 
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) > 2:
+        try:
+            host = sys.argv[1]
+            port = int(sys.argv[2])
+            if port > 65535 or port < 1:
+                print("[ERR] Invalid port number.")
+                exit()
+        except Exception as e:
+            print(f"[ERR] {e}")
+        except KeyboardInterrupt:
+            print("[ERR] : Disconnected from host")
+            exit()
+
+        main(port, host)
+    else:
+        print("Arguement missing: python3 listener.py <host address> <portnumber>")
