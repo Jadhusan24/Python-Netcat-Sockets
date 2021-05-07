117091346500d9e0e0dd40465c4c55548c77a82f
Jadhusan-Sadhik <54207086+Jadhusan-S@users.noreply.github.com>
2021-05-07 12:10:41 +0530

    Update listener.py

--- listener.py
+++ listener.py
@@ -1,33 +1,53 @@
-#!/usr/bin/python
-#COHNDNE201f-021
-
+#!/usr/bin/python3
 import socket
-import threading
-from module import handle_conn, send_message, ADDR
+import threading, time
+from module import handle_conn, send_message, get_ip
+import sys
 
-#https://www.techwithtim.net/tutorials/socket-programming/
-def main():
+# gets the IP address of the current machine
+SERVER = get_ip()
+
+
+def main(port: int):
     try:
-        # create socket
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server.bind(ADDR)  # bind socket to port and server
-        server.listen()  # start listening
-
+        server.bind((SERVER, port))
+        server.listen()
+        print(f"Listening on {SERVER}:{port}")
         conn, addr = server.accept()  # accept 1 connection
+        get_msg = threading.Thread(
+            target=handle_conn, args=(conn, addr), daemon=True)
+        send_msg = threading.Thread(
+            target=send_message, args=(conn,))
 
-        # Here, I used threading to asynchronously receive and send data between the client and server.
-        # Threading simply creates a separate set of threads in memory. This helps the script to run without
-        # having to wait for loops to end
-        get_msg = threading.Thread(target=handle_conn,args=(conn, addr))  # this thread is used for receiving messages/data
-        send_msg = threading.Thread(target=send_message,args=(conn,))  # this thread is used for sending messages/data
-
-        # start both threads
+        get_msg.setDaemon(True)
+        send_msg.setDaemon(True)
         get_msg.start()
         send_msg.start()
+        while True:
+            for _ in range(10):
+                time.sleep(.2)
+    except KeyboardInterrupt:
+        print("\nEnding server")
+        exit(1)
     except Exception as e:
         print(f"[ERR] {e}")
-        pass
+        return
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        if len(sys.argv) > 1:
+            port = int(sys.argv[1])
+            if port > 65535 or port < 1:
+                print("[ERR] Invalid port number.")
+                exit()
+            main(port)
+        else:
+            print("Arguement missing: python3 listener.py <portnumber>")
+    except Exception as e:
+        print(f"[ERR] {e}")
+        exit()
+    except KeyboardInterrupt:
+        print("[ERR] : Stopping listener")
+        exit()
